ad78a4325fe38354a029246313de2e91f7dee083
Itay <itay.bittan@gmail.com>
2012-05-02 22:19:01 +0300

    transaction sorter

---
+++ FinancePlus/PersistentLayer/TransactionsSorter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FinancePlus.PersistentLayer
+{
+    class TransactionsSorter : IComparer<Transaction>
+    {
+        public int Compare(Transaction t1, Transaction t2)
+        {
+            return t1.getChargeDate().CompareTo(t2.getChargeDate());
+            //return obj2.CompareTo(obj1);
+        }
+    }
+}
